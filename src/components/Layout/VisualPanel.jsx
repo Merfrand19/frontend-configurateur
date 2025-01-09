ddc6ac4d6b9e9ec1b80c4e793f39007fc32aa1e1
Merfrand <126403434+Merfrand19@users.noreply.github.com>
2025-01-09 17:57:02 +0100

    Update VisualPanel.jsx

--- src/components/Layout/VisualPanel.jsx
+++ src/components/Layout/VisualPanel.jsx
@@ -1,64 +1,3 @@
-// import React from 'react';
-// import logo from '../../assets/logo.png';
-// import bgGarage from '../../assets/bg-garage.png';
-// import { useLayoutContext } from '../../context/appContext';
-// import ButtonExpand from '../UI/buttonExpand';
-// import SubmitButton from '../UI/submitButton';
-
-
-// const VisualPanel = ({ setIsVisualExpanded, isVisualExpanded }) => {
-    
-//     const {brand,identity,bike } =useLayoutContext()
-    
-//   return (
-//     <div
-//       className="bg-cover bg-center h-full w-full"
-//       style={{ backgroundImage: `url(${bgGarage})` }}
-//     >   
-//         <div className='flex flex-col items-center text-center h-full w-full relative'>
-//             <div className="w-1/2 z-10 pt-16 pb-16 flex justify-center">
-//                 <img src={logo} alt="Logo" className='w-1/2'/>
-//             </div>
-//             <div className='flex flex-col items-center w-full bottom-0'>
-//                 <div className='flex flex-col items-center relative'>
-//                     <p className="text-bl-doe absolute top-6 text-xl font-medium">{identity}</p>
-//                     <h1 className=" text-black text-gradient absolute top-10" >{brand}</h1>
-//                     <div className="flex justify-center z-10">
-//                         <img src={bike} alt="Bike" className="bike md:w-[100%] lg:w-[80%]" />
-//                     </div>
-//                    <div className='flex justify-space-beetween '>
-//                       <ButtonExpand setIsVisualExpanded={setIsVisualExpanded} isVisualExpanded={isVisualExpanded} />
-//                       {isVisualExpanded && <SubmitButton />}
-//                    </div>
-//                 </div>
-//             </div>
-//         </div>    
-//     </div>
-//   );
-// };
-
-// export default VisualPanel;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 import React from 'react';
 import logo from '../../assets/logo.png';
 import bgGarage from '../../assets/bg-garage.png';
@@ -71,7 +10,8 @@ const VisualPanel = ({ triggerFormSubmit, setIsVisualExpanded, isVisualExpanded 
 
   return (
     <div
-      className="bg-cover bg-center h-full w-full"
+{/*       className="bg-cover bg-center h-full w-full" */}
+    className={`bg-cover bg-center w-full h-full ${isVisualExpanded ? 'h-screen md:h-full' : ''}`}
       style={{ backgroundImage: `url(${bgGarage})` }}
     >
       <div className="flex flex-col items-center text-center h-full w-full relative">
