c3d6caa18c757cd106dd504ccec36d2abc72a05b
Merfrand LOKONON <gratiaslokonon@gmail.com>
2025-01-21 23:40:59 +0100

    dotenv added

--- api/shopify.js
+++ api/shopify.js
@@ -3,7 +3,9 @@ import 'dotenv/config'; // Ajoutez ceci tout en haut du fichier
 export default async function handler(req, res) {
   const API_URL = process.env.VITE_SHOPIFY_GRAPHQL_URI;
   const ACCESS_TOKEN = process.env.VITE_SHOPIFY_STOREFRONT_ACCESS_TOKEN;
-
+     console.log('API_URL:', API_URL);
+     console.log('ACCESS_TOKEN:', ACCESS_TOKEN);
+     
   if (!API_URL || !ACCESS_TOKEN) {
     return res.status(500).json({ error: 'Missing API URL or access token' });
   }
