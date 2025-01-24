aac132d4399b911bad511d339ae22faa5b46622c
Merfrand LOKONON <gratiaslokonon@gmail.com>
2025-01-24 19:16:21 +0100

    suppression des commentaires

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,3 @@
-// import { useState } from 'react'
-// import AppLayout from './components/Layout/AppLayout'
-// import VisualPanel from './components/Layout/VisualPanel'
-// import FormPanel from './components/Layout/FormPanel'
-// import { AppContextProvider } from './context/appContext'
-// import { ApolloProvider, useQuery } from '@apollo/client';
-// import client from './services/ApolloClient'; 
-
-// import '@fontsource/poppins'
-
-
-// function App() {
-//   const [count, setCount] = useState(0)
-//   const [isVisualExpanded, setIsVisualExpanded] = useState(false);
-
-//   return (
-//     <AppContextProvider>
-//       <AppLayout 
-//         visualPanel={<VisualPanel setIsVisualExpanded={setIsVisualExpanded} isVisualExpanded={isVisualExpanded} />} 
-//         formPanel={<FormPanel/>}
-//         isVisualExpanded={isVisualExpanded}
-//       />
-        
-//     </AppContextProvider>  
-//   )
-// }
-
-// export default App
-
-
 import { useState, useEffect } from 'react';
 import AppLayout from './components/Layout/AppLayout';
 import VisualPanel from './components/Layout/VisualPanel';
